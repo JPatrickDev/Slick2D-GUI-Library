74bf110625410656f751aad07b8456200e45e5eb
Jack Patrick <jackpatrickmail@gmail.com>
2018-05-02 01:15:36 +0100

    Implemented a 9-patch creator.

--- src/com/jdp30/gui/Example/ExampleGame.java
+++ src/com/jdp30/gui/Example/ExampleGame.java
@@ -3,7 +3,10 @@ package com.jdp30.gui.Example;
 import com.jdp30.gui.Elements.TextArea;
 import com.jdp30.gui.GUIArea;
 import com.jdp30.gui.util.BasicGUIGame;
+import com.jdp30.gui.util.NinePatchBuilder;
 import org.newdawn.slick.*;
+
+import java.util.Random;
 
 /**
  * Created by Jack on 01/05/2018.
@@ -20,6 +23,7 @@ public class ExampleGame extends BasicGUIGame {
         agc.start();
     }
 
+    Image i = null, ii = null;
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
         GUIArea area = new GUIArea(0, 0, gameContainer.getWidth(), gameContainer.getHeight());
@@ -27,16 +31,28 @@ public class ExampleGame extends BasicGUIGame {
         for (int i = 0; i != 15; i++)
             area.addElement(new TextArea("Hello World!", 0, i * 30, area.getWidth(), 30, Color.magenta, Color.orange));
 
-        addGUIArea(area);
+    //    addGUIArea(area);
+
+        Random r = new Random();
+
+        int w =r.nextInt(350);
+        i = NinePatchBuilder.getImage(new Image("res/button.png"),6,6,w,(w/3) + r.nextInt(20));
+        w =r.nextInt(350);
+        ii = NinePatchBuilder.getImage(new Image("res/button.png"),6,6,w,(w/3) + r.nextInt(20));
     }
 
+
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
-        super.update(gameContainer, i);
+    public void update(GameContainer gameContainer, int fi) throws SlickException {
+        super.update(gameContainer, fi);
+
+
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
         super.render(gameContainer, graphics);
+        graphics.drawImage(i,0,0);
+        graphics.drawImage(ii,0,i.getHeight());
     }
 }
---
+++ src/com/jdp30/gui/util/NinePatchBuilder.java
@@ -0,0 +1,69 @@
+package com.jdp30.gui.util;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.ImageBuffer;
+import org.newdawn.slick.SlickException;
+
+/**
+ * Created by Jack on 01/05/2018.
+ */
+public class NinePatchBuilder {
+
+    public static Image getImage(Image patch, int patchW, int patchH, int targetWidth, int targetHeight) {
+        long start = System.currentTimeMillis();
+        Image[][] patches = new Image[3][3];
+        for (int x = 0; x != 3; x++) {
+            for (int y = 0; y != 3; y++) {
+                patches[x][y] = patch.getSubImage(x * patchW, y * patchH, patchW, patchH);
+            }
+        }
+        ImageBuffer buffer = new ImageBuffer(targetWidth, targetHeight);
+
+
+        for (int x = 1; x != (targetWidth / patchW); x++) {
+            for (int y = 1; y != (targetHeight / patchH); y++) {
+                drawImage(patches[1][1], x * patchW, y * patchH, buffer);
+            }
+        }
+
+
+        for (int x = 0; x <= (int) targetWidth / patchW; x++) {
+            drawImage(patches[1][0], x * patchW, 0, buffer);
+            drawImage(patches[1][2], x * patchW, targetHeight - patchH, buffer);
+        }
+
+        for (int y = 0; y <= (int) targetHeight / patchH; y++) {
+            drawImage(patches[0][1], 0, y * patchH, buffer);
+            drawImage(patches[2][1], targetWidth - patchW, y * patchH, buffer);
+        }
+
+
+        drawImage(patches[0][0], 0, 0, buffer);
+        drawImage(patches[2][0], targetWidth - patchW, 0, buffer);
+
+        drawImage(patches[0][2], 0, targetHeight - patchH, buffer);
+        drawImage(patches[2][2], targetWidth - patchW, targetHeight - patchH, buffer);
+
+        long diff = System.currentTimeMillis() - start;
+        System.out.println("Time: " + diff);
+
+        return buffer.getImage();
+    }
+
+    private static void drawImage(Image i, int x, int y, ImageBuffer buffer) {
+        if (x >= buffer.getWidth() || y >= buffer.getHeight())
+            return;
+        try {
+            for (int iX = 0; iX != i.getWidth(); iX++) {
+                for (int iY = 0; iY != i.getHeight(); iY++) {
+                    Color c = i.getColor(iX, iY);
+                    buffer.setRGBA(x + iX, y + iY, c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha());
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+
+}
